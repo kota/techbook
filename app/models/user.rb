09e9ecce11886e92e299ca49adcab3172cf2e0f9
kota <fujiwara.kota@gmail.com>
2012-12-26 04:24:05 +0900

    bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :omniauthable
+  devise :database_authenticatable,:omniauthable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :github_user_id, :name
   # attr_accessible :title, :body
 
   def self.find_or_create_from_auth_hash(auth_hash)
